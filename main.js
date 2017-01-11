aceb4049b8b33cd1befd82c2811df21cc61a44d5
illona fedora <superlhona@gmail.com>
2017-01-11 08:55:29 +0700

    Add function to navbar when scrolling

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
  $(document).ready(function(){
 
+  // navbar
+  $(window).scroll(function(){
+    if($(window).scrollTop() > 300 && $("nav").css("position") != "fixed"){
+      $("nav").hide(function(){
+        $("nav").css("position", "fixed");
+        $("nav").css("background-color", "#9B6D5C");
+        $("nav").css("width", "100%");
+        $("nav").css("z-index", "1999");
+        $("ul").css("float", "right");
+        $("nav").slideDown('200');
+      });
+    } else if($(window).scrollTop() <= 300 ){
+      $("nav").css("position", "initial");
+      $("nav").css("background-color", "initial");
+      $("nav").css("width", "initial");
+    }
+  });
+
 });
